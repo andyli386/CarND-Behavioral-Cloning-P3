fcc87234c2600ca84f67ac844a14e3b9d57b0b10
Vincent <andyli386@gmail.com>
2017-11-13 20:55:54 +0800

    add drive.py

--- drive.py
+++ drive.py
@@ -44,10 +44,10 @@ class SimplePIController:
 
 
 controller = SimplePIController(0.1, 0.002)
-set_speed = 9
+set_speed = 30
 controller.set_desired(set_speed)
 
-
+from scipy.misc import imresize
 @sio.on('telemetry')
 def telemetry(sid, data):
     if data:
@@ -61,6 +61,7 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
+        #image_array = imresize(image_array, (80, 160, 3))
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
